fc0feeec12cbb02d477a44cf31dc449d0e5f47ec
Jonathanko52 <jonathan.ko523@gmail.com>
2025-12-15 18:34:01 -0800

    added comments

--- app/api/googleAuth/route.ts
+++ app/api/googleAuth/route.ts
@@ -5,22 +5,25 @@ import { google } from "googleapis";
 
 export async function POST(req: Request) {
   // Optional: read dynamic range from request
-  const body = await req.json().catch(() => ({}));
-  const range = body.range || "Jobs!A2:B2";
 
+  // Credentials
   const rawCreds = process.env.GOOGLE_SERVICE_ACCOUNT_JSON;
   if (!rawCreds) throw new Error("GOOGLE_SERVICE_ACCOUNT_JSON missing");
-
   const creds = JSON.parse(rawCreds);
   creds.private_key = creds.private_key.replace(/\\n/g, "\n");
 
+  // Authentication object
   const auth = new google.auth.GoogleAuth({
     credentials: creds,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
+  // Sheets
   const sheets = google.sheets({ version: "v4", auth });
 
+  // Calling spreadsheet and using range
+  const body = await req.json().catch(() => ({}));
+  const range = body.range || "Jobs!A2:B2";
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: "1DcNybZwq7WrXw-AwWCGpPzQf6mDYKAbm1Co3U882gGQ",
     range,
