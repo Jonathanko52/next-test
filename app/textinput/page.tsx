940d8e199f21c99e637a94baf93e29a6633172c6
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-01 15:44:25 -0800

    Buttons having issues

--- app/textinput/page.tsx
+++ app/textinput/page.tsx
@@ -96,7 +96,7 @@ export default function TextboxWithButton() {
         </button>
         <button
           type="button"
-          className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+          className="bg-success"
           style={{
             width: "600px",
             height: "40px",
