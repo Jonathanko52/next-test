eb0cb92be66e06f55870ca997b3d6f293e4a40e6
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-07 18:05:31 -0800

    console logs added for testing

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,7 +23,8 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-
+  console.log("PATHNAME", pathname);
+  console.log("HREF", link.href);
   return (
     <>
       {links.map((link) => {
