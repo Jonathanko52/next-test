4ed55384f76eae5cfe48b3683feaa03fea39eda1
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-13 14:28:10 -0800

    trimmed functions

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -11,61 +11,7 @@ export default function TodoMain() {
   const [postingLink, setPostingLink] = useState<string | null>(null);
   const [submitted, setSubmitted] = useState(false);
 
-  const scrapeAndSubmit = async () => {
-    //First call. Retrieve the company name, position, link and such from linkedin.
-    const resOne = await fetch("/api/linkedInGet", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ value }),
-    });
-
-    //Convert the retrieved data into Json
-    const dataOne = await resOne.json();
-
-    setCompanyName(dataOne.companyName);
-    setJobPosting(dataOne.jobPosting);
-    setLocation(dataOne.location);
-
-    //Second call. Send data to google sheets to be written.
-    const resTwo = await fetch("/api/moddedSheetsPost", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ dataOne }),
-    });
-
-    //If successful, resets all values on page.
-    if (resTwo.status == 200) {
-      setValue("Successfully submitted!");
-      setSubmitted(false);
-      setCompanyName(dataOne.companyName);
-      setJobPosting(dataOne.jobPosting);
-      setLocation(dataOne.location);
-      setPostingLink("");
-    }
-  };
-
-  const test = async () => {
-    //First call. Retrieve the company name, position, link and such from linkedin.
-    const resOne = await fetch("/api/linkedInGet", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ value }),
-    });
-
-    //Convert the retrieved data into Json
-    const dataOne = await resOne.json();
-
-    setSubmitted(false);
-    setCompanyName(dataOne.companyName);
-    setJobPosting(dataOne.jobPosting);
-    setLocation(dataOne.location);
-  };
+  const test = async () => {};
 
   return <div></div>;
 }
