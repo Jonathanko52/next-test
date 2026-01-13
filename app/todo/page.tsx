cc6a416b14dc903e31a110705d89211bad5a4b18
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-13 14:28:24 -0800

    trimmed state

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -6,10 +6,6 @@ import { Button } from "@/app/ui/button";
 export default function TodoMain() {
   const [value, setValue] = useState("");
   const [companyName, setCompanyName] = useState<string | null>(null);
-  const [jobPosting, setJobPosting] = useState<string | null>(null);
-  const [location, setLocation] = useState<string | null>(null);
-  const [postingLink, setPostingLink] = useState<string | null>(null);
-  const [submitted, setSubmitted] = useState(false);
 
   const test = async () => {};
 
