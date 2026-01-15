1d23133d44631e06826f09475cf7152893b85370
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-15 14:06:47 -0800

    Right sidebar modified

--- app/todo/components/RightSidebar.tsx
+++ app/todo/components/RightSidebar.tsx
@@ -3,11 +3,7 @@ import Link from "next/link";
 export default function Sidebar() {
   return (
     <aside className="w-64 border-r p-4">
-      <nav className="space-y-2">
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/dashboard/reports">Reports</Link>
-        <Link href="/dashboard/settings">Settings</Link>
-      </nav>
+      Ideally some kind of chat window
     </aside>
   );
 }
