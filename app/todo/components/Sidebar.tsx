e2ff6ad592cea6f4ccfd26cb9bb261aa762f651b
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-15 13:49:10 -0800

    Created stuff for left sidebar

--- app/todo/components/Sidebar.tsx
+++ app/todo/components/Sidebar.tsx
@@ -4,9 +4,12 @@ export default function Sidebar() {
   return (
     <aside className="w-64 border-r p-4">
       <nav className="space-y-2">
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/dashboard/reports">Reports</Link>
-        <Link href="/dashboard/settings">Settings</Link>
+        <Link href="/dashboard">Goals</Link>
+        <br></br>
+        <Link href="/dashboard">Monthly Target</Link>
+        <br></br>
+        <Link href="/dashboard">Milestone</Link>
+        <br></br>
       </nav>
     </aside>
   );
