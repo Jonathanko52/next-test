88dc067d6df17fc89ebd7af938b610323bb8cdc8
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-16 17:58:44 -0800

    This can get complicated

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -7,11 +7,15 @@ export default function TodoMain() {
   const [todoItems, setToDoItems] = useState([]);
   const [companyName, setCompanyName] = useState<string | null>(null);
 
+  const displayToDo = () => {};
+  const changeStateOfItem = () => {};
+
   const test = async () => {};
 
   //todoitem structure:
   /*
   {
+    id:
     priority: 1~99  
     taskHeader: text
     taskText: text
