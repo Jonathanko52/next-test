fcca09d4f0231e0d956f1b8d2414ea7aefaf9591
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-16 17:53:16 -0800

    Started on functionality of todo

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Button } from "@/app/ui/button";
 
 export default function TodoMain() {
-  const [value, setValue] = useState("");
+  const [todoItems, setToDoItems] = useState([]);
   const [companyName, setCompanyName] = useState<string | null>(null);
 
   const test = async () => {};
