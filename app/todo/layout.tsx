89c53da3dbd80967506cd84e87cdd14b76b1ceb0
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-19 14:32:32 -0800

    Working on layout

--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -11,10 +11,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Header></Header>
-      <RightSidebar></RightSidebar>
-      <Sidebar></Sidebar>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <div className="grid min-h-screen grid-cols-[240px_1fr_240px] grid-rows-[auto_1fr]">
+          {/* Header (center only) */}
+          <div className="col-start-2 row-start-1">
+            <Header />
+          </div>
+
+          {/* Left sidebar */}
+          <div className="col-start-1 row-span-2">
+            <Sidebar />
+          </div>
+
+          {/* Page content */}
+          <main className="col-start-2 row-start-2 p-4">{children}</main>
+
+          {/* Right sidebar */}
+          <div className="col-start-3 row-span-2">
+            <RightSidebar />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
