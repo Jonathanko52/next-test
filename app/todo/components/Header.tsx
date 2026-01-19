d9b1e084ab3f27eb91a15db9d391bd8fe059a260
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-19 14:50:40 -0800

    Header CSS

--- app/todo/components/Header.tsx
+++ app/todo/components/Header.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/app/ui/button";
 
 export default function Header() {
   return (
-    <header className="h-14 border-b px-4 flex items-center">
+    <header className="h-14 border-b px-4 ml-12 flex items-center">
       <h1 className="font-semibold">HEADER</h1>
     </header>
   );
