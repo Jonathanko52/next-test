33ecb3deb7337d80754f419367bc7b420c5674a7
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-20 12:56:18 -0800

    Adding main

---
+++ app/todo/components/GoalsWindow.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/app/ui/button";
+
+export default function Header() {
+  return (
+    <header className="h-14 border-b px-4">
+      <h1 className="font-semibold">Main</h1>
+    </header>
+  );
+}
