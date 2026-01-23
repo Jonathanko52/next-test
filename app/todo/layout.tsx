7929a50cae5a2c4fce76e31c6e5d535eda63a1ad
Jonathanko52 <jonathan.ko523@gmail.com>
2026-01-23 13:32:11 -0800

    added provider and wrapped the component

--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -8,6 +8,7 @@ import TextboxWithButton from "./../textinput/page";
 import Header from "./components/Header";
 import { Button } from "@/app/ui/button";
 import { useState } from "react";
+import { StateProvider } from "@/context/StateContext";
 
 export default function RootLayout({
   children,
@@ -68,101 +69,103 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <div className="grid min-h-screen grid-cols-[240px_1fr_240px] grid-rows-[auto_1fr]">
-          <div className="col-start-2 row-start-1">
-            <Header />
+        <StateProvider>
+          <div className="grid min-h-screen grid-cols-[240px_1fr_240px] grid-rows-[auto_1fr]">
+            <div className="col-start-2 row-start-1">
+              <Header />
+            </div>
+            <div className="col-start-1 row-span-2">
+              <Sidebar />
+            </div>
+            <main className="col-start-2 row-start-2 p-4">{children}</main>
+            {buttonDiv ? (
+              <div>
+                <Button
+                  className="bg-success justify-center"
+                  style={{
+                    width: "200px",
+                    height: "40px",
+                    fontSize: "1.2em",
+                    margin: "1em",
+                    marginLeft: "2em",
+                  }}
+                  onClick={toggleRightSidebar}>
+                  RightSidebar
+                </Button>
+                <Button
+                  className="bg-success justify-center"
+                  style={{
+                    width: "200px",
+                    height: "40px",
+                    fontSize: "1.2em",
+                    margin: "1em",
+                    marginLeft: "2em",
+                  }}
+                  onClick={toggleScraperSidebar}>
+                  Job Scraper
+                </Button>
+              </div>
+            ) : null}
+            {jobScraperOpen ? (
+              <div className="col-start-3 row-span-2">
+                <TextboxWithButton />{" "}
+                <Button
+                  className="bg-success justify-center"
+                  style={{
+                    width: "200px",
+                    height: "40px",
+                    fontSize: "1.2em",
+                    margin: "1em",
+                    marginLeft: "2em",
+                  }}
+                  onClick={toggleRightSidebar}>
+                  RightSidebar
+                </Button>
+                <Button
+                  className="bg-success justify-center"
+                  style={{
+                    width: "200px",
+                    height: "40px",
+                    fontSize: "1.2em",
+                    margin: "1em",
+                    marginLeft: "2em",
+                  }}
+                  onClick={toggleScraperSidebar}>
+                  Job Scraper
+                </Button>
+              </div>
+            ) : null}
+            {rightSidebarOpen ? (
+              <div className="col-start-3 row-span-2">
+                <RightSidebar />{" "}
+                <Button
+                  className="bg-success justify-center"
+                  style={{
+                    width: "200px",
+                    height: "40px",
+                    fontSize: "1.2em",
+                    margin: "1em",
+                    marginLeft: "2em",
+                  }}
+                  onClick={toggleRightSidebar}>
+                  RightSidebar
+                </Button>
+                <Button
+                  className="bg-success justify-center"
+                  style={{
+                    width: "200px",
+                    height: "40px",
+                    fontSize: "1.2em",
+                    margin: "1em",
+                    marginLeft: "2em",
+                  }}
+                  onClick={toggleScraperSidebar}>
+                  Job Scraper
+                </Button>
+              </div>
+            ) : null}
           </div>
-          <div className="col-start-1 row-span-2">
-            <Sidebar />
-          </div>
-          <main className="col-start-2 row-start-2 p-4">{children}</main>
-          {buttonDiv ? (
-            <div>
-              <Button
-                className="bg-success justify-center"
-                style={{
-                  width: "200px",
-                  height: "40px",
-                  fontSize: "1.2em",
-                  margin: "1em",
-                  marginLeft: "2em",
-                }}
-                onClick={toggleRightSidebar}>
-                RightSidebar
-              </Button>
-              <Button
-                className="bg-success justify-center"
-                style={{
-                  width: "200px",
-                  height: "40px",
-                  fontSize: "1.2em",
-                  margin: "1em",
-                  marginLeft: "2em",
-                }}
-                onClick={toggleScraperSidebar}>
-                Job Scraper
-              </Button>
-            </div>
-          ) : null}
-          {jobScraperOpen ? (
-            <div className="col-start-3 row-span-2">
-              <TextboxWithButton />{" "}
-              <Button
-                className="bg-success justify-center"
-                style={{
-                  width: "200px",
-                  height: "40px",
-                  fontSize: "1.2em",
-                  margin: "1em",
-                  marginLeft: "2em",
-                }}
-                onClick={toggleRightSidebar}>
-                RightSidebar
-              </Button>
-              <Button
-                className="bg-success justify-center"
-                style={{
-                  width: "200px",
-                  height: "40px",
-                  fontSize: "1.2em",
-                  margin: "1em",
-                  marginLeft: "2em",
-                }}
-                onClick={toggleScraperSidebar}>
-                Job Scraper
-              </Button>
-            </div>
-          ) : null}
-          {rightSidebarOpen ? (
-            <div className="col-start-3 row-span-2">
-              <RightSidebar />{" "}
-              <Button
-                className="bg-success justify-center"
-                style={{
-                  width: "200px",
-                  height: "40px",
-                  fontSize: "1.2em",
-                  margin: "1em",
-                  marginLeft: "2em",
-                }}
-                onClick={toggleRightSidebar}>
-                RightSidebar
-              </Button>
-              <Button
-                className="bg-success justify-center"
-                style={{
-                  width: "200px",
-                  height: "40px",
-                  fontSize: "1.2em",
-                  margin: "1em",
-                  marginLeft: "2em",
-                }}
-                onClick={toggleScraperSidebar}>
-                Job Scraper
-              </Button>
-            </div>
-          ) : null}
-        </div>
+        </StateProvider>
       </body>
     </html>
   );
