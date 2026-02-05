4779a49c45f857a9e12f4191417c759fb7b809ad
Jonathanko52 <jonathan.ko523@gmail.com>
2026-02-05 13:41:51 -0800

    learned about arrange

--- numpyExercises.py
+++ numpyExercises.py
@@ -149,3 +149,15 @@ four_by_five[:,0]
 # 27. What does print(four_by_five[:, 3:]) display
 
 four_by_five[:,3:]
+
+one_dim = np.arange(1,6)
+
+# 28. What would be displayed by print(one_dim * 2)
+
+one_dim * 2
+
+# 29. What would be returned by this expression: one_dim + np.arange(5, 0, -1) ?
+
+one_dim + np.arange(5, 0, -1)
+
+print(np.arange(5, 0,0.5))
