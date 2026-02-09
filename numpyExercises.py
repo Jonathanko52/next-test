c232af0b1ed1e5dac8168ede8ffefb581c05586b
Jonathanko52 <jonathan.ko523@gmail.com>
2026-02-08 21:03:14 -0800

    around is for rounding to decimals

--- numpyExercises.py
+++ numpyExercises.py
@@ -206,9 +206,11 @@ one_dim + np.arange(5, 0, -1)
 
 arr = np.array([0., .5, 1.0, 1.5, 2.0]) * np.pi
 
-# What does the following display:
+# # What does the following display:
 
-print(np.int32(np.sin(arr)))
+# print(np.int32(np.sin(arr)))
 
 
+# 37. For the defined above in #36, what are the values for:
 
+print(np.around(np.cos(arr), 0))
