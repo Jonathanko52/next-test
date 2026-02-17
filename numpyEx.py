61a1fbde1f413a41ba117b317415d1de1788747b
Jonathanko52 <jonathan.ko523@gmail.com>
2026-02-16 16:12:41 -0800

    First index

--- numpyEx.py
+++ numpyEx.py
@@ -88,6 +88,12 @@ new_array = original_array.reshape(3, 4)
 
 # 16. Given new_array from the last exercise, and the code x = new_array, you run the code:
 
+x = new_array
+
+new_array[0,0] = 25
+
+print(x[0,0])
+
 # 17. How could you create a two-dimensional, 3 x 4 array (three arrays of four elements each) with random numbers from 1 to 10?
 
 # 18. How could you create an array of the same size and shape as #17, filled with 64-bit integer zeros?
