7b0d94207f8f1da1ebb0ad8372d3f4dd2e0f8a47
Jonathanko52 <jonathan.ko523@gmail.com>
2026-02-25 15:36:13 -0800

    adding state to item

--- app/todoitem/page.tsx
+++ app/todoitem/page.tsx
@@ -1,10 +1,8 @@
-import AcmeLogo from "@/app/ui/acme-logo";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import styles from "@/app/ui/home.module.css";
-import { lusitana } from "@/app/ui/fonts";
-import Image from "next/image";
+"use client";
 
+import { useState } from "react";
+import { Button } from "@/app/ui/button";
+import { useGlobalState } from "@/app/context/StateContext";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
