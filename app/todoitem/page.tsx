e8cff444330ddaebbfd6c00552db1ac7d83d38d9
Jonathanko52 <jonathan.ko523@gmail.com>
2026-02-26 22:32:47 -0800

    Java setters and getters logic?

--- app/todoitem/page.tsx
+++ app/todoitem/page.tsx
@@ -29,6 +29,33 @@ export default function Page() {
   }
   */
 
+  const setter = () => 
+    { itemId, setItemId 
+
+    }
+  const setter = () => 
+    { 
+
+    }
+  const setter = () => 
+    { taskText, setTaskText 
+
+    }
+  const setter = () => 
+    { taskState, setTaskState 
+
+    }
+  const setter = () => 
+    { subtasks, setSubtasks 
+
+    }
+  const setter = () => 
+    { repeatability:, setRepeatability 
+
+    }
+
+  
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"></div>
