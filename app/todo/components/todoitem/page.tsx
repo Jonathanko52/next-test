84453a07611d7ae5bfad17d43a9b2493c264c822
Jonathanko52 <jonathan.ko523@gmail.com>
2026-03-03 18:47:24 -0800

    Header adn Text

--- app/todo/components/todoitem/page.tsx
+++ app/todo/components/todoitem/page.tsx
@@ -77,7 +77,8 @@ export default function Page() {
       <div
         className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"
         onClick={setterEditMode}>
-        TASK ITEM
+        {taskHeader}
+        {taskText}
       </div>
       :
       <div
