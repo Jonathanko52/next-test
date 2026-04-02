4ab5f8e57cc7b529e28260261141c604667dfada
Jonathanko52 <jonathan.ko523@gmail.com>
2026-04-02 15:00:39 -0700

    fix icon

--- app/todo/components/todoitem/page.tsx
+++ app/todo/components/todoitem/page.tsx
@@ -117,6 +117,7 @@ export default function Page() {
           id={itemId}
           className="flex flex-col h-auto rounded-lg bg-blue-500 p-4  text-white">
           <div onClick={taskStateHandlerOutsideEdit}>{iconMap[taskState]}</div>
+          <br></br>
           <div onClick={handlerEditMode}>
             <div className="font-bold border-b border-blue-400 pb-2">
               Header: {taskHeader}
